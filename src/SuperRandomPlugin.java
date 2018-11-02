72e89c7b4f7e4a809e648ba58c04e3f957679558
Everett Flynn <44507725+OutmodedChip64@users.noreply.github.com>
2018-11-02 07:58:37 -0700

    Updated SuperRandomPlugin.java to v1.4

--- src/SuperRandomPlugin.java
+++ src/SuperRandomPlugin.java
@@ -1,6 +1,9 @@
 package com.flynntech;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -14,16 +17,21 @@ import org.bukkit.inventory.PlayerInventory;
 public final class SuperRandomPlugin extends org.bukkit.plugin.java.JavaPlugin
 {
 	FileConfiguration config = getConfig();
+	private static Map<UUID, ItemStack[]> hashMapInv = new HashMap<>();
+	private static Map<UUID, ItemStack[]> hashMapArm = new HashMap<>();
+	private static UUID[] peopleFilledInv = new UUID[1];
+	private static boolean lastPeopleArrayUsed = true;
 	
   public SuperRandomPlugin() {}
   
   public void onEnable()
   {
-    getLogger().info("Welcome to SuperRandomPlugin v1.3.2!");
+    getLogger().info("Welcome to SuperRandomPlugin v1.4!");
     getLogger().info("Checking config...");
     checkConfig();
     getLogger().info("SuperRandomPlugin has been enabled");
-    getLogger().info("[ChangeLog] Fixed a small issue in the last version");
+    getLogger().info("[ChangeLog] Added new commands, /fillinv and /undoinv");
+    getLogger().info("[ChangeLog] Small fixes");
   }
   
   public void onDisable()
@@ -43,7 +51,9 @@ public final class SuperRandomPlugin extends org.bukkit.plugin.java.JavaPlugin
         }
         target.getWorld().createExplosion(target.getLocation(), 0.0F);
         target.setHealth(0.0D);
-        target.sendMessage("You have been exploded by: " + sender.getName());
+        sender.sendMessage("You have exploded " + args[0]);
+        target.sendMessage("You have been exploded by " + sender.getName());
+        return true;
     } else if (cmd.getName().equalsIgnoreCase("explode") && !config.getBoolean("canUseExplode")) {
     	sender.sendMessage(config.getString("messageIfCannotUse"));
     	return true;
@@ -58,6 +68,7 @@ public final class SuperRandomPlugin extends org.bukkit.plugin.java.JavaPlugin
         pos.setY(255.0D);
         target.teleport(pos);
         target.sendMessage("You have been skyed by: " + sender.getName());
+        return true;
     } else if (cmd.getName().equalsIgnoreCase("sky") && !config.getBoolean("canUseSky")) {
        	sender.sendMessage(config.getString("messageIfCannotUse"));
     	return true;
@@ -88,6 +99,8 @@ public final class SuperRandomPlugin extends org.bukkit.plugin.java.JavaPlugin
     	World world = target.getWorld();
     	Location location = target.getLocation();
     	world.strikeLightning(location);
+    	sender.sendMessage("You have struck " + args[0] + " with lightning.");
+    	target.sendMessage("You have been struck with lightning by " + sender.getName());
     	return true;
     } else if (cmd.getName().equalsIgnoreCase("lightningstr") && !config.getBoolean("canUseLightningstr")) {
     	sender.sendMessage(config.getString("messageIfCannotUse"));
@@ -108,6 +121,90 @@ public final class SuperRandomPlugin extends org.bukkit.plugin.java.JavaPlugin
     } else if (cmd.getName().equalsIgnoreCase("dirt") && !config.getBoolean("canUseDirt")) {
     	sender.sendMessage(config.getString("messageIfCannotUse"));
     	return true;
+    } else if (cmd.getName().equalsIgnoreCase("fillinv") && args.length == 2 && config.getBoolean("canUsefillinv")) {
+    	Player target = sender.getServer().getPlayer(args[0]);
+    	if (target == null) {
+    		sender.sendMessage(args[0] + " is currently not online.");
+    		return true;
+    	}
+    	int tempInt = 0;
+    	boolean foundIndex = false;
+    	while (tempInt < peopleFilledInv.length) {
+        	if (peopleFilledInv[tempInt] == target.getUniqueId()) {
+    			foundIndex = true;
+    		}
+    		tempInt++;
+    	}
+    	if (foundIndex) {
+    		sender.sendMessage("You have already used /fillinv on this player!");
+    		return true;
+    	}
+    	PlayerInventory inventory = target.getInventory();
+		ItemStack itemstack = new ItemStack(org.bukkit.Material.getMaterial(args[1]), 2304);
+		hashMapInv.put(target.getUniqueId(), inventory.getContents());
+		hashMapArm.put(target.getUniqueId(), inventory.getArmorContents());
+		UUID[] tempArray = new UUID[peopleFilledInv.length];
+		if (peopleFilledInv[peopleFilledInv.length - 1] == null) {
+			peopleFilledInv[peopleFilledInv.length - 1] = target.getUniqueId();
+		} else {
+			if (lastPeopleArrayUsed) {
+				tempInt = 0;
+				while (tempInt != tempArray.length) {
+					tempArray[tempInt] = peopleFilledInv[tempInt];
+					tempInt++;
+				}
+				peopleFilledInv = new UUID[tempArray.length + 1];
+				peopleFilledInv[peopleFilledInv.length - 1] = target.getUniqueId();
+			} else {
+				peopleFilledInv[peopleFilledInv.length - 1] = target.getUniqueId();
+			}
+		}
+		
+		inventory.clear();
+		inventory.addItem(new ItemStack[] { itemstack });
+		target.sendMessage("Your inventory has been filled with the item '" + args[1] + "' by " + sender.getName());
+		sender.sendMessage("You filled " + args[0] + "'s inventory with the item '" + args[1] + "'");
+    	return true;
+    } else if (cmd.getName().equalsIgnoreCase("fillinv") && !config.getBoolean("canUsefillinv")) {
+    	sender.sendMessage(config.getString("messageIfCannotUse"));
+    	return true;
+    } else if (cmd.getName().equalsIgnoreCase("undoinv") && config.getBoolean("canUseundoinv")) {
+    	Player player = sender.getServer().getPlayer(sender.getName());
+    	int tempInt = 0;
+    	int index = 0;
+    	boolean foundIndex = false;
+    	while (tempInt < peopleFilledInv.length) {
+        	if (peopleFilledInv[tempInt] == player.getUniqueId()) {
+    			foundIndex = true;
+    			index = tempInt;
+    		}
+    		tempInt++;
+    	}
+    	if (foundIndex == false) {
+    		sender.sendMessage("Nobody has used /fillinv on you!");
+    		return true;
+    	}
+    	tempInt = 0;
+    	while (tempInt <= peopleFilledInv.length) {
+    		if (index >= tempInt) {
+    			if (index == peopleFilledInv.length - 1) {
+    				peopleFilledInv[peopleFilledInv.length - 1] = null;
+    				tempInt = peopleFilledInv.length + 1;
+    			} else {
+    				peopleFilledInv[index] = peopleFilledInv[index + 1];
+    			}
+    		}
+    		tempInt++;
+    	}
+    	PlayerInventory inventory = player.getInventory();
+    	inventory.clear();
+    	inventory.setContents(hashMapInv.get(player.getUniqueId()));
+    	inventory.setArmorContents(hashMapArm.get(player.getUniqueId()));
+    	player.sendMessage("You have restored your inventory!");
+    	return true;
+    } else if (cmd.getName().equalsIgnoreCase("undoinv") && !config.getBoolean("canUseundoinv")) {
+    	sender.sendMessage(config.getString("messageIfCannotUse"));
+    	return true;
     }
     return false;
   }
@@ -121,18 +218,28 @@ public final class SuperRandomPlugin extends org.bukkit.plugin.java.JavaPlugin
 		  if (!file.exists()) {
 			  getLogger().info("config.yml not found! Creating...");
 			  saveDefaultConfig();
-			  config.set("pluginVersion", "v1.3.2");
+			  config.set("pluginVersion", "v1.4");
 			  config.set("canUseExplode", true);
 			  config.set("canUseSky", true);
 			  config.set("canUseCmdblock", true);
 			  config.set("canUseLightningstr", true);
 			  config.set("canUseDirt", true);
+			  config.set("canUsefillinv", true);
+			  config.set("canUseundoinv", true);
 			  config.set("messageIfCannotUse", "That command has been turned off.");
 			  saveConfig();
-		  } else if (!(config.getString("pluginVersion") == "v1.3.2") || config.getString("pluginVersion") == null) {
+		  } else if (config.getString("pluginVersion") == null) {
 			  getLogger().info("config.yml found, outdated! Updating...");
-			  config.set("pluginVersion", "v1.3.2");
+			  config.set("pluginVersion", "v1.4");
 			  config.set("canUseDirt", true);
+			  config.set("canUsefillinv", true);
+			  config.set("canUseundoinv", true);
+			  saveConfig();
+		  } else if (!(config.getString("pluginVersion") == "v1.4")) {
+			  getLogger().info("config.yml found, outdated! Updating...");
+			  config.set("pluginVersion", "v1.4");
+			  config.set("canUsefillinv", true);
+			  config.set("canUseundoinv", true);
 			  saveConfig();
 		  } else {
 			  getLogger().info("config.yml found! Loading...");
